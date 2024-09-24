a04b7e47612f1461f5bcc37f079dddf010e15400
iXZed026 <danyal.titanka24@gmail.com>
2024-09-24 14:57:04 +0330

    add token data

--- src/context/TokensProvider.js
+++ src/context/TokensProvider.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { createContext, useState } from 'react';
 
-const TokensProvider = () => {
-  return (
-    <div>TokensProvider</div>
-  )
+const TokensContext = createContext();
+const SetTokensContext = createContext();
+
+const TokensProvider = ({ children }) => {
+
+    const [tokens, setTokens] = useState();
+
+    return (
+        <TokensContext.Provider value={tokens}>
+            <SetTokensContext.Provider value={setTokens}>
+                {children}
+            </SetTokensContext.Provider>
+        </TokensContext.Provider>
+    )
 }
 
+export { TokensContext, SetTokensContext }
 export default TokensProvider
