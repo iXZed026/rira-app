1ccbe07e666a5c2ab65d6ebbaa438a751db3d076
iXZed026 <danyal.titanka24@gmail.com>
2024-09-24 15:10:53 +0330

    change header folder

---
+++ src/component/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header>
+            <div className="header-container">
+                <div className="header-flex">
+                    <div className="header-right-items">
+                        <div className="header-logo"></div>
+                        
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
