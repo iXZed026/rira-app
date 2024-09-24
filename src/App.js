67c59ad95c36633ba263e5e4826f73c2785c2cc3
iXZed026 <danyal.titanka24@gmail.com>
2024-09-24 14:33:47 +0330

    create tokens provider

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <div>app</div>
   )
 }
 
---
+++ src/context/TokensProvider.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TokensProvider = () => {
+  return (
+    <div>TokensProvider</div>
+  )
+}
+
+export default TokensProvider
