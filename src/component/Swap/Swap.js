7d6cacb70b562afb4b056b49116d52a326f122c6
iXZed026 <danyal.titanka24@gmail.com>
2024-09-27 11:44:33 +0330

    create swap component

---
+++ src/component/Swap/Swap.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Swap.css";
+
+const Swap = () => {
+    return (
+        <div className="swap">
+            <div className="swap-container">
+                <div className="swap-flex">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Swap
