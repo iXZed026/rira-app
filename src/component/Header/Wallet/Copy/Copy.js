5cd26f7cf33c0eb7d37eb5027b52ec8c2225ebc3
iXZed026 <danyal.titanka24@gmail.com>
2024-09-27 10:41:41 +0330

    add copy component

---
+++ src/component/Header/Wallet/Copy/Copy.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Copy.css";
+
+const Copy = () => {
+    return (
+        <div>Copy</div>
+    )
+}
+
+export default Copy
--- src/component/Header/Wallet/DisconnectWallet/DisconnectWallet.js
+++ src/component/Header/Wallet/DisconnectWallet/DisconnectWallet.js
@@ -18,6 +18,19 @@ const DisconnectWallet = (props) => {
         }, 750)
     }
 
+    //Disconnect wallet opration
+    const disconnectWalletHandler = async () => {
+        closeModalHandler()
+        setWalletId(null);
+    }
+
+    //Copy wallet id
+    const handleCopyText = () => {
+        if (walletId) {
+            navigator.clipboard.writeText(walletId)
+        }
+    }
+
     return (
         <div className="disconnect-wallet">
             <div className="disconnect-wallet-container" ref={modalRef}>
@@ -25,15 +38,15 @@ const DisconnectWallet = (props) => {
                     <IoCloseSharp onClick={closeModalHandler} />
                     <div className="disconnect-wallet-body">
                         <div id='disconnect-wallet-picture'><img src="../../../images/alienLogo.png" alt="!Image not found" /></div>
-                        <h3>{walletId}</h3>
+                        <h3>{walletId ? walletId : "Disconnected"}</h3>
                     </div>
                     <div className="disconnect-wallet-footer">
-                        <button><RiLogoutBoxRLine />Disconnect</button>
-                        <button><MdOutlineContentCopy />Copy</button>
+                        <button onClick={disconnectWalletHandler}><RiLogoutBoxRLine />لغو اتصال</button>
+                        <button><MdOutlineContentCopy onClick={handleCopyText} />کپی</button>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
