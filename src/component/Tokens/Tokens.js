2d2f2ca308f9e33725457c588d957951bf20a201
iXZed026 <danyal.titanka24@gmail.com>
2024-09-29 13:09:55 +0330

    create tokens component

---
+++ src/component/Tokens/Tokens.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Tokens.css";
+
+const Tokens = () => {
+  return (
+    <div className="tokens">
+        <div className="tokens-container">
+            <div className="tokens-flex">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Tokens
